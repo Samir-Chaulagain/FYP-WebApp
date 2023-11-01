7618af109469bc28caa0d940c93ddbbdcf98597f
royal chaulagain <royal.chaulagain321@gmail.com>
2023-11-01 23:43:02 +0545

    Added exploreitem page

--- museRental/main/urls.py
+++ museRental/main/urls.py
@@ -1,17 +1,26 @@
 from django.urls import path
-
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("signup", views.signup, name="signup"),
-    path("blog", views.blog, name="blog"),
-    path("contact", views.contact, name="contact"),
-    path("about", views.about, name="about"),
+    path("signup/", views.signup, name="signup"),
+    path("blog/", views.blog, name="blog"),
+    path("contact/", views.contact, name="contact"),
+    path("about/", views.about, name="about"),
      path('post/<str:pk>', views.post, name='post'), 
+
+
+
+
+    path('items/', views.items, name='items'),
+    path('items/new/', views.new, name='new'),
+    path('items/<int:pk>/', views.detail, name='detail'),
+    path('items/<int:pk>/delete/', views.delete, name='delete'),
+    path('items/<int:pk>/edit/', views.edit, name='edit'),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- museRental/main/views.py
+++ museRental/main/views.py
@@ -1,5 +1,5 @@
 
-from .models import Post
+
 import readline
 from django.conf import settings
 from django.http import HttpResponse
@@ -14,6 +14,12 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_str, force_bytes
 from . tokens import generate_token
 from readline import get_current_history_length
+
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import NewItemForm, EditItemForm
+from .models import Category, Item,Post
 
 def index(request):
     return render(request, 'landing_pages/index.html')
@@ -102,3 +108,81 @@ def signup(request):
 def post(request, pk):
     posts = Post.objects.get(id=pk)
     return render(request, 'posts.html', {'posts': posts})
+
+
+
+
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'landing_pages/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id)
+    })
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+
+    return render(request, 'landing_pages/detail.html', {
+        'item': item,
+        'related_items': related_items
+    })
+
+
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('main:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    return render(request, 'landing_pages/form.html', {
+        'form': form,
+        'title': 'New item',
+    })
+
+
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('main:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'landing_pages/form.html', {
+        'form': form,
+        'title': 'Edit item',
+    })
+
+
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('index')
+    
