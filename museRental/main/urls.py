fd9adfa19b9b34958dce923c828bbb49d0182631
Samir Chaulagain <royal.chaulagain321@gmail.com>
2023-11-03 23:11:14 +0545

    Update .gitignore

--- museRental/main/urls.py
+++ museRental/main/urls.py
@@ -5,10 +5,10 @@ from django.conf import settings
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
     
     path("blog/", views.blog, name="blog"),
     path("contact/", views.contact, name="contact"),
-    path("about/", views.about, name="about"),
     path('post/<str:pk>', views.post, name='post'), 
     path('items/', views.items, name='items'),
     path('items/new/', views.new, name='new'),
