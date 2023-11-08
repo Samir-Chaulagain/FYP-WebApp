6f2f08543c659a4915b44f55d1f4e586ddd4b30e
Samir Chaulagain <royal.chaulagain321@gmail.com>
2023-11-08 23:37:08 +0545

    changes in Files

--- museRental/accounts/views.py
+++ museRental/accounts/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-
+# Customer and Owner signup login
 
 def SignupPage(request):
     if request.method=='POST':
