79f4b2b79dfe004154527b4fcbe94e88f91f3d80
Samir Chaulagain <royal.chaulagain321@gmail.com>
2023-11-13 12:33:39 +0545

    changes

--- museRental/explore/views.py
+++ museRental/explore/views.py
@@ -78,47 +78,4 @@ def delete(request, pk):
 
     return redirect('index')
 
-def upload_instrument(request):
-    instrument_name=request.POST.get('instrument_name','')
-    instrument_company=request.POST.get('instrument_company','')
-    instrument_model=request.POST.get('instrument_model','')
-    instrument_type=request.POST.get('instrument_type','')
-    instrument_fuel=request.POST.get('instrument_fuel','')
-    instrument_No_of_Seats=request.POST.get('instrument_No_of_Seats','')
-    instrument_color=request.POST.get('instrument_color','')
-    instrument_license_plate=request.POST.get('instrument_license_plate','')
-    
-    instrument_uploaded_by=request.session.get('user_email')
 
-    isGeared=request.POST.get('isGeared','')
-    instrument_description=request.POST.get('instrument_description','')
-    instrument_price=request.POST.get('instrument_price','')
-    instrument_image1=request.FILES['instrument_image1']
-    instrument_image2=request.FILES['instrument_image2']
-    instrument_image3=request.FILES['instrument_image3']
-
-    result_instrument = instrument.objects.filter(instrument_license_plate=instrument_license_plate)
-    result_owner = Owner.objects.filter(Owner_email=instrument_uploaded_by)
-    result_manager = Manager.objects.filter(Manager_email=instrument_uploaded_by)
-
-    if result_instrument.exists():
-        if result_owner.exists():
-            Message = "This instrument already exist!!"
-            return render(request,'Owner_Upload_instrument.html',{'Message':Message})
-        if result_manager.exists():
-            Message = "This instrument already exist!!"
-            return render(request,'Manager_Upload_instrument.html',{'Message':Message})
-    else:
-        instrument=instrument(instrument_name=instrument_name,instrument_company=instrument_company,
-        instrument_model=instrument_model,instrument_type=instrument_type,instrument_fuel=instrument_fuel,
-        instrument_No_of_Seats=instrument_No_of_Seats,instrument_color=instrument_color,
-        instrument_license_plate=instrument_license_plate,
-        instrument_uploaded_by=instrument_uploaded_by,isGeared=isGeared,instrument_description=instrument_description,
-        instrument_price=instrument_price,instrument_image1=instrument_image1,instrument_image2=instrument_image2,
-        instrument_image3=instrument_image3)
-        
-        instrument.save()
-        if result_owner.exists():
-            return redirect('/Owner/Allinstruments')
-        if result_manager.exists():
-            return redirect('/Manager/Allinstruments')
